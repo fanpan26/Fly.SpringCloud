2380a367f324f1c60c88efeca51dee619cddc944
fanpan26 <fanpan26@126.com>
2019-03-01 23:09:46 +0800

    restTemplate

--- services/fly-user-service/src/main/java/com/fyp/fly/services/user/UserServiceApplication.java
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/UserServiceApplication.java
@@ -3,6 +3,9 @@ package com.fyp.fly.services.user;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author fyp
@@ -16,5 +19,11 @@ public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class,args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
