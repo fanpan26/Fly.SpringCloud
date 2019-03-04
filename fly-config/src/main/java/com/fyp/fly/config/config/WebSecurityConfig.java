1343240ad6b2d469974fdc3d77afc86cd93f3a6c
fanpan26 <fanpan26@126.com>
2019-03-04 19:23:12 +0800

    Update WebSecurityConfig.java

--- fly-config/src/main/java/com/fyp/fly/config/config/WebSecurityConfig.java
+++ fly-config/src/main/java/com/fyp/fly/config/config/WebSecurityConfig.java
@@ -13,10 +13,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
-        http.csrf().disable();
-        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
-        // 如果是form方式,不能使用url格式登录
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+       http.csrf().disable();
+//        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+//        // 如果是form方式,不能使用url格式登录
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 }
