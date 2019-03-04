f0a20947c6ed036a5461d27cd8ada55e21fcf3e7
fanpan26 <fanpan26@126.com>
2019-03-04 18:49:15 +0800

    config csrf

--- fly-config/src/main/java/com/fyp/fly/config/config/WebSecurityConfig.java
+++ fly-config/src/main/java/com/fyp/fly/config/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
@@ -12,7 +13,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //开启认证
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        // 如果是form方式,不能使用url格式登录
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 }
