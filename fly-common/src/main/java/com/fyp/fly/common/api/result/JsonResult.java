86e73aac60afab25d76ced1079aa10e2f0bc8acf
fanpan26 <fanpan26@126.com>
2019-03-05 23:50:46 +0800

    add jsonresult

---
+++ fly-common/src/main/java/com/fyp/fly/common/api/result/JsonResult.java
@@ -0,0 +1,49 @@
+package com.fyp.fly.common.api.result;
+
+/**
+ * @author fyp
+ * @crate 2019/3/5 22:28
+ * @project fly
+ */
+public class JsonResult<T> {
+
+    public JsonResult(int code, String msg, T data){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public JsonResult(int code, String msg){
+        this(code,msg,null);
+    }
+
+    public JsonResult(int code){
+        this(code,null,null);
+    }
+
+    public JsonResult(){
+        this(0,"ok",null);
+    }
+    public JsonResult(T data){
+        this(0,"ok",data);
+    }
+
+    private int code;
+    private String msg;
+
+    public T getData() {
+        return data;
+    }
+
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+}
