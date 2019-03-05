6d0356993ef6a706cb42fb9b33076cb48e8e8bdc
fanpan26 <fanpan26@126.com>
2019-03-05 23:51:30 +0800

    Create Account.java

---
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/domain/Account.java
@@ -0,0 +1,47 @@
+package com.fyp.fly.services.account.domain;
+
+/**
+ * @author fyp
+ * @crate 2019/3/5 22:56
+ * @project fly
+ */
+public class Account {
+    public static final String ERROR_MSG_NOT_EXIST = "no account exists";
+    public static final String ERROR_MSG_WRONG_PWD = "wrong password";
+
+    public static boolean notExists(final Account account){
+        return account == null;
+    }
+
+    public boolean isCorrectPassword(final String userInputPwd){
+        return loginPwd.equals(userInputPwd);
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getLoginPwd() {
+        return loginPwd;
+    }
+
+    public void setLoginPwd(String loginPwd) {
+        this.loginPwd = loginPwd;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String loginName;
+    private String loginPwd;
+    private long id;
+}
