f26f6644cd80877758cf99730274b4c3e6813ef9
fanpan26 <fanpan26@126.com>
2019-03-05 23:51:15 +0800

    add MapperScan

--- services/fly-account-service/src/main/java/com/fyp/fly/services/account/AccountApplication.java
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/AccountApplication.java
@@ -1,10 +1,12 @@
 package com.fyp.fly.services.account;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.fyp.fly.services.account.repositories.mapper")
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
