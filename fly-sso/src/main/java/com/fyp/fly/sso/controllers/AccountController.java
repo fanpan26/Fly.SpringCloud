80be8d712c889a9940fdf507b76ba3c88a8f01aa
fanpan26 <fanpan26@126.com>
2019-03-05 17:26:25 +0800

    sso resources

---
+++ fly-sso/src/main/java/com/fyp/fly/sso/controllers/AccountController.java
@@ -0,0 +1,15 @@
+package com.fyp.fly.sso.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
