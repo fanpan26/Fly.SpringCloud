46e0635046f43eb2bd163b9ce4058d2c0975d4a0
fanpan26 <fanpan26@126.com>
2019-03-06 14:28:46 +0800

    Update SsoApplication.java

--- fly-sso/src/main/java/com/fyp/fly/sso/SsoApplication.java
+++ fly-sso/src/main/java/com/fyp/fly/sso/SsoApplication.java
@@ -1,11 +1,35 @@
 package com.fyp.fly.sso;
 
+import com.fyp.fly.sso.api.client.AccountApiClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SsoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SsoApplication.class,args);
     }
+
+    @Bean
+    public RestTemplate getRestTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(3000);
+        return factory;
+    }
+
+    @Bean
+    public AccountApiClient getAccountApiClient(){
+        return new AccountApiClient();
+    }
+
 }
