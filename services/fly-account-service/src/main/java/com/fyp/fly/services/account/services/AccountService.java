cbc136abd8ecc58ba440de0ef5cb08bdc2369cb6
fanpan26 <fanpan26@126.com>
2019-03-06 09:56:10 +0800

    add doc

--- services/fly-account-service/src/main/java/com/fyp/fly/services/account/services/AccountService.java
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/services/AccountService.java
@@ -3,5 +3,14 @@ package com.fyp.fly.services.account.services;
 import com.fyp.fly.common.api.result.JsonResult;
 
 public interface AccountService {
+    /**
+     * 用户登录接口，用于 fly-sso 调用
+     *
+     * @param loginName 用户名
+     * @param loginPwd  密码
+     * @return 登录成功：返回用于web站点校验的ticket 登录失败：账号不存在/密码错误
+     */
     JsonResult login(String loginName, String loginPwd);
+
+    JsonResult verifyTicket(String ssoKey);
 }
