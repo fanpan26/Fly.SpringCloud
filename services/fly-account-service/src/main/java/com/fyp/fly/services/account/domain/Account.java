63b1e0dc81de6fecd53d62cf78eab736d40a31b7
fanpan26 <fanpan26@126.com>
2019-03-06 14:29:30 +0800

    add type

--- services/fly-account-service/src/main/java/com/fyp/fly/services/account/domain/Account.java
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/domain/Account.java
@@ -6,8 +6,6 @@ package com.fyp.fly.services.account.domain;
  * @project fly
  */
 public class Account {
-    public static final String ERROR_MSG_NOT_EXIST = "no account exists";
-    public static final String ERROR_MSG_WRONG_PWD = "wrong password";
 
     public static boolean notExists(final Account account){
         return account == null;
@@ -49,5 +47,49 @@ public class Account {
     public String toString(){
         return "loginName:"+loginName
                 +".loginPwd:"+loginPwd;
+    }
+
+    public enum AccountType {
+
+        /**
+         * 正常
+         */
+        OK(0, "ok"),
+        /**
+         * 账户不存在
+         */
+        NOT_EXISTS(10001, "账户不存在"),
+        /**
+         * 密码不正确
+         */
+        WRONG_PASSWORD(10002, "密码错误"),
+        /**
+         * 密码错误次数超出限制
+         */
+        WRONG_PASSWORD_LIMIT(10003, "密码错误次数过多，账号已暂时锁定，请稍候在试"),
+        /**
+         * 账户已经被锁定
+         */
+        LOCKED(10004, "账号已暂时锁定，请稍后再试"),
+        /**
+         * 账号被拉黑
+         * */
+        BLACKED(10005,"账号已被禁止登录");
+
+        AccountType(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        private int code;
+        private String msg;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
     }
 }
