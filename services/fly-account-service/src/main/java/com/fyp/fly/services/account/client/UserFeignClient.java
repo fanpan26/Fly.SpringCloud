7f5f74b6932ae60c60dcddb9292dec48a8777a38
fanpan26 <fanpan26@126.com>
2019-03-11 01:25:58 +0800

    Create UserFeignClient.java

---
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/client/UserFeignClient.java
@@ -0,0 +1,22 @@
+package com.fyp.fly.services.account.client;
+
+import com.fyp.fly.common.dto.FlyUserDto;
+import com.fyp.fly.common.result.api.JsonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author fyp
+ * @crate 2019/3/10 23:45
+ * @project fly
+ */
+@FeignClient(name = "fly-user-service")
+public interface UserFeignClient {
+    /**
+     * 获取用户信息
+     * */
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
+    JsonResult<FlyUserDto> getUserById(@PathVariable("id") Long id);
+}
