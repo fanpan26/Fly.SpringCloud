7e9ebfbcd091e5ba856f0e5fa5b050d77252cc91
fanpan26 <fanpan26@126.com>
2019-03-10 23:28:54 +0800

    Update AccountApiClient.java

--- fly-sso/src/main/java/com/fyp/fly/sso/api/client/AccountApiClient.java
+++ fly-sso/src/main/java/com/fyp/fly/sso/api/client/AccountApiClient.java
@@ -3,6 +3,7 @@ package com.fyp.fly.sso.api.client;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.common.result.api.SsoTicketApiResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,12 +16,12 @@ import org.springframework.web.client.RestTemplate;
  * @author fanpan26
  * */
 public final class AccountApiClient {
+
     /**
      * eureka [account service] id:fly-account-service
      * */
     private static final String API_ACCOUNT_SERVICE_ID = "fly-account-service";
 
-    private static final String API_ACCOUNT_SERVICE_HOST = "http://localhost:8082/";
     private static final String API_LOGIN = "/account/login";
     private static final String API_TICKET = "/account/ticket";
     private static final String API_TICKET_VERIFY = "/account/ticket/verify";
@@ -30,14 +31,23 @@ public final class AccountApiClient {
         POST_FORM_URLENCODED_HEADER.add("Content-Type", "application/x-www-form-urlencoded");
     }
 
+    @Value("${fly.sso.gateway_url}")
+    private String gateWayHost;
+
     @Autowired
     private RestTemplate restTemplate;
 
 
-    protected String getApiUrl(String url){
-        return String.format("%s%s",API_ACCOUNT_SERVICE_HOST,url);
+    /**
+     * 构建请求地址。ex: http://localhost:9001/fly-account-service/account/login
+     * */
+    protected String getApiUrl(String address){
+        return String.format("%s%s%s",gateWayHost, API_ACCOUNT_SERVICE_ID,address);
     }
 
+    /**
+     * 登录
+     * */
     public JsonResult<SsoTicketApiResult> login(String name, String pwd) {
         MultiValueMap<String, Object> parameter = new LinkedMultiValueMap<>();
         parameter.add("name", name);
@@ -49,18 +59,17 @@ public final class AccountApiClient {
         return result;
     }
 
+    /**
+     *
+     * */
     public JsonResult<SsoTicketApiResult> getTicketByToken(String token) {
-        MultiValueMap<String, Object> parameter = new LinkedMultiValueMap<>();
-        parameter.add("token", token);
-        return postForObjectWithFormHeader(getApiUrl(API_TICKET), parameter, new ParameterizedTypeReference<JsonResult<SsoTicketApiResult>>() {
+        return postForObject(getApiUrl(API_TICKET) + "?token=" + token, new ParameterizedTypeReference<JsonResult<SsoTicketApiResult>>() {
 
         });
     }
 
     public JsonResult<SsoTicketApiResult> verifyTicket(String ticket) {
-        MultiValueMap<String, Object> parameter = new LinkedMultiValueMap<>();
-        parameter.add("ticket", ticket);
-        return postForObjectWithFormHeader(getApiUrl(API_TICKET_VERIFY), parameter, new ParameterizedTypeReference<JsonResult<SsoTicketApiResult>>() {
+        return postForObject(getApiUrl(API_TICKET_VERIFY) + "?ticket=" + ticket, new ParameterizedTypeReference<JsonResult<SsoTicketApiResult>>() {
 
         });
     }
@@ -71,6 +80,11 @@ public final class AccountApiClient {
     private  <T> JsonResult<T> postForObjectWithFormHeader(String apiUrl, MultiValueMap<String, Object> parameters, ParameterizedTypeReference<JsonResult<T>> parameterizedTypeReference) {
         HttpEntity<MultiValueMap<String, Object>> requestBody = new HttpEntity<>(parameters, POST_FORM_URLENCODED_HEADER);
         JsonResult<T> apiResult = restTemplate.exchange(apiUrl, HttpMethod.POST, requestBody, parameterizedTypeReference).getBody();
+        return apiResult;
+    }
+
+    private  <T> JsonResult<T> postForObject(String apiUrl, ParameterizedTypeReference<JsonResult<T>> parameterizedTypeReference) {
+        JsonResult<T> apiResult = restTemplate.exchange(apiUrl, HttpMethod.POST, null, parameterizedTypeReference).getBody();
         return apiResult;
     }
 
