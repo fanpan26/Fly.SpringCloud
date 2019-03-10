dbb0078180a01798597b997a23404a1fcab664fd
fanpan26 <fanpan26@126.com>
2019-03-10 22:11:22 +0800

    zuul application

---
+++ fly-gateway/src/main/java/com/fyp/fly/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.fyp.fly.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author fyp
+ * @crate 2019/3/10 20:54
+ * @project fly
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
