15331abd261ee9547ff693148a27d1eb84b22dc4
fanpan26 <fanpan26@126.com>
2019-03-11 01:25:54 +0800

    add @EnableFeignClients

--- services/fly-account-service/src/main/java/com/fyp/fly/services/account/AccountApplication.java
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/AccountApplication.java
@@ -4,8 +4,10 @@ package com.fyp.fly.services.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 @MapperScan("com.fyp.fly.services.account.repositories.mapper")
 public class AccountApplication {
     public static void main(String[] args) {
