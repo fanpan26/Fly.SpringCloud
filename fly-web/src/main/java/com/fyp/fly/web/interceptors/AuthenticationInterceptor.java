7b9282dea7dcbfb6d6968263f6c5e499f1a8e85c
fanpan26 <fanpan26@126.com>
2019-03-11 01:25:30 +0800

    Update AuthenticationInterceptor.java

--- fly-web/src/main/java/com/fyp/fly/web/interceptors/AuthenticationInterceptor.java
+++ fly-web/src/main/java/com/fyp/fly/web/interceptors/AuthenticationInterceptor.java
@@ -1,8 +1,20 @@
 package com.fyp.fly.web.interceptors;
 
 import com.fyp.fly.common.constants.Fly;
+import com.fyp.fly.common.dto.FlyUserDto;
+import com.fyp.fly.common.result.api.JsonResult;
+import com.fyp.fly.common.result.api.ResultUtils;
+import com.fyp.fly.common.result.api.SsoTicketApiResult;
+import com.fyp.fly.common.tools.EncodeUtils;
+import com.fyp.fly.common.tools.JSONUtils;
+import com.fyp.fly.web.utils.CookieUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.Cookie;
@@ -16,27 +28,52 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Component
 public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
+
+    public AuthenticationInterceptor(){
+        System.out.println("AuthenticationInterceptor created");
+    }
+    @Value("${sso.url}")
+    private String ssoUrl;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
-        String token = getAuthenticationToken(request);
+        String token = CookieUtils.getCookie(request, Fly.WEB_COOKIE_KEY);
         if (!StringUtils.isEmpty(token)) {
-
+            String userJson = CookieUtils.getCookie(request, Fly.WEB_COOKIE_USER_KEY);
+            if (!StringUtils.isEmpty(userJson)) {
+                FlyUserDto user = JSONUtils.parseObject(EncodeUtils.decodeBase64(userJson), FlyUserDto.class);
+                if (user != null) {
+                    request.setAttribute(Fly.WEB_ATTRIBUTE_USER_KEY, user);
+                    return true;
+                }
+            }
+            //if no user info,check token
+            JsonResult<FlyUserDto> userRes = getUserFromSsoApi(token);
+            if (userRes != null) {
+                String userCookieString = EncodeUtils.encodeBase64(JSONUtils.toJSONString(userRes.getData()));
+                CookieUtils.setCookie(response, Fly.WEB_COOKIE_USER_KEY, userCookieString , Fly.WEB_COOKIE_USER_EXPIRE);
+                request.setAttribute(Fly.WEB_ATTRIBUTE_USER_KEY, userRes.getData());
+            }
         }
         return true;
     }
 
-    protected String getAuthenticationToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null) {
-            return null;
+    private JsonResult<FlyUserDto> getUserFromSsoApi(String token) {
+        JsonResult<FlyUserDto> response = restTemplate.exchange(ssoUrl + "/user/info?token=" + token,
+                HttpMethod.GET, null, new ParameterizedTypeReference<JsonResult<FlyUserDto>>() {
+                }).getBody();
+        if (ResultUtils.isSuccess(response)) {
+            return response;
         }
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(Fly.WEB_COOKIE_KEY)) {
-                return cookie.getValue();
-            }
-        }
+
+        //TODO log the response error code and msg
         return null;
     }
+
 }
