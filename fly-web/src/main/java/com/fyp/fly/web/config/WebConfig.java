df4f147850269ee06f5a5b6dc3bfa61496ce8d9f
fanpan26 <fanpan26@126.com>
2019-03-10 23:29:20 +0800

    Create WebConfig.java

---
+++ fly-web/src/main/java/com/fyp/fly/web/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.fyp.fly.web.config;
+
+import com.fyp.fly.web.interceptors.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author fyp
+ * @crate 2019/3/10 23:21
+ * @project fly
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationInterceptor interceptor;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**")
+                .addResourceLocations("classpath:/templates/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/res/css/**")
+                .excludePathPatterns("/res/images/**")
+                .excludePathPatterns("/res/layui/**");
+    }
+}
