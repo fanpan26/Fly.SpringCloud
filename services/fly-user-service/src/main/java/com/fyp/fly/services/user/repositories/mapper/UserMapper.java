8a91418d4de4998714307e6b2b285b381d5a5177
fanpan26 <fanpan26@126.com>
2019-03-13 21:22:27 +0800

    Create UserMapper.java

---
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/repositories/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.fyp.fly.services.user.repositories.mapper;
+
+import com.fyp.fly.services.user.domain.FlyUser;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author fyp
+ * @crate 2019/3/13 19:51
+ * @project fly
+ */
+public interface UserMapper {
+    /**
+     * 获取基础用户信息
+     * */
+    @Select("select name,avatar,sex,vip,auth,address,sign,auth_str,create_at from fly_user where id=#{id}")
+    @Results({
+            @Result(property = "authStr",  column = "auth_str")
+    })
+    FlyUser getUserById(@Param("id") Long id);
+}
