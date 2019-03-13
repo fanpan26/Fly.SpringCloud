dd759c24a5e1a0a0985df76212a37b3d496dafcc
fanpan26 <fanpan26@126.com>
2019-03-13 21:22:31 +0800

    Create DefaultUserService.java

---
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/services/DefaultUserService.java
@@ -0,0 +1,56 @@
+package com.fyp.fly.services.user.services;
+
+import com.fyp.fly.common.constants.Fly;
+import com.fyp.fly.common.result.api.JsonResult;
+import com.fyp.fly.common.result.api.ResultUtils;
+import com.fyp.fly.common.tools.JSONUtils;
+import com.fyp.fly.services.user.cache.Cache;
+import com.fyp.fly.services.user.domain.FlyUser;
+import com.fyp.fly.services.user.domain.dto.FlyUserBaseInfoDto;
+import com.fyp.fly.services.user.repositories.mapper.UserMapper;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author fyp
+ * @crate 2019/3/13 20:13
+ * @project fly
+ */
+@Service
+public class DefaultUserService implements UserService {
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    private ValueOperations<String, String> ops() {
+        return redisTemplate.opsForValue();
+    }
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    private FlyUser getUserFromCache(Long userId){
+        FlyUser user;
+        String userJson = ops().get(Cache.CACHE_USER+userId);
+        if (StringUtils.isEmpty(userJson)){
+           user = userMapper.getUserById(userId);
+           ops().set(Cache.CACHE_USER+userId,JSONUtils.toJSONString(user),Cache.CACHE_USER_EXPIRE, TimeUnit.SECONDS);
+        }else{
+            user = JSONUtils.parseObject(userJson,FlyUser.class);
+        }
+
+        return user;
+    }
+
+    @Override
+    public JsonResult<FlyUserBaseInfoDto> getUserBaseInfo(Long userId) {
+        FlyUser user = getUserFromCache(userId);
+        return ResultUtils.success(FlyUserBaseInfoDto.createByUser(user));
+    }
+}
