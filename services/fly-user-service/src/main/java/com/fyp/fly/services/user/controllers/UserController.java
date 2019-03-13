6ef54fb6aa78cff319b7f1dfbe2566043e7934b8
fanpan26 <fanpan26@126.com>
2019-03-13 21:22:12 +0800

    Update UserController.java

--- services/fly-user-service/src/main/java/com/fyp/fly/services/user/controllers/UserController.java
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/controllers/UserController.java
@@ -2,14 +2,13 @@ package com.fyp.fly.services.user.controllers;
 
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.common.result.api.ResultUtils;
-import com.fyp.fly.services.user.domain.UserInfo;
+import com.fyp.fly.services.user.services.UserService;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author fyp
@@ -20,9 +19,11 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/{id}")
     public JsonResult getUserInfoById(@PathVariable Long id){
-        return ResultUtils.success(new UserInfo());
+       return userService.getUserBaseInfo(id);
     }
 }
