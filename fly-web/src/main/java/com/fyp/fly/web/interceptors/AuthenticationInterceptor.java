6281103df9b9430a812c9eb4a321b40024a67491
fanpan26 <fanpan26@126.com>
2019-03-13 21:21:29 +0800

    add redis

--- fly-web/src/main/java/com/fyp/fly/web/interceptors/AuthenticationInterceptor.java
+++ fly-web/src/main/java/com/fyp/fly/web/interceptors/AuthenticationInterceptor.java
@@ -4,22 +4,22 @@ import com.fyp.fly.common.constants.Fly;
 import com.fyp.fly.common.dto.FlyUserDto;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.common.result.api.ResultUtils;
-import com.fyp.fly.common.result.api.SsoTicketApiResult;
-import com.fyp.fly.common.tools.EncodeUtils;
 import com.fyp.fly.common.tools.JSONUtils;
 import com.fyp.fly.web.utils.CookieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author fyp
@@ -35,6 +35,17 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
     @Value("${sso.url}")
     private String ssoUrl;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    private ValueOperations<String, String> ops() {
+        return redisTemplate.opsForValue();
+    }
+
+    private String cacheKey(HttpServletRequest request){
+        return Fly.WEB_CACHE_USER_KEY+request.getSession().getId();
+    }
+
 
     @Autowired
     private RestTemplate restTemplate;
@@ -45,9 +56,9 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
 
         String token = CookieUtils.getCookie(request, Fly.WEB_COOKIE_KEY);
         if (!StringUtils.isEmpty(token)) {
-            String userJson = CookieUtils.getCookie(request, Fly.WEB_COOKIE_USER_KEY);
+            String userJson = ops().get(cacheKey(request));
             if (!StringUtils.isEmpty(userJson)) {
-                FlyUserDto user = JSONUtils.parseObject(EncodeUtils.decodeBase64(userJson), FlyUserDto.class);
+                FlyUserDto user = JSONUtils.parseObject(userJson, FlyUserDto.class);
                 if (user != null) {
                     request.setAttribute(Fly.WEB_ATTRIBUTE_USER_KEY, user);
                     return true;
@@ -56,8 +67,8 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
             //if no user info,check token
             JsonResult<FlyUserDto> userRes = getUserFromSsoApi(token);
             if (userRes != null) {
-                String userCookieString = EncodeUtils.encodeBase64(JSONUtils.toJSONString(userRes.getData()));
-                CookieUtils.setCookie(response, Fly.WEB_COOKIE_USER_KEY, userCookieString , Fly.WEB_COOKIE_USER_EXPIRE);
+                String userJsonString = JSONUtils.toJSONString(userRes.getData());
+                ops().set(cacheKey(request),userJsonString,Fly.WEB_CACHE_USER_EXPIRE, TimeUnit.SECONDS);
                 request.setAttribute(Fly.WEB_ATTRIBUTE_USER_KEY, userRes.getData());
             }
         }
@@ -71,7 +82,6 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
         if (ResultUtils.isSuccess(response)) {
             return response;
         }
-
         //TODO log the response error code and msg
         return null;
     }
