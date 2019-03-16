edfae61de67c54355c96186e330a7ee20e64aef9
fanpan26 <fanpan26@126.com>
2019-03-16 15:02:57 +0800

    Create ArticleApiClient.java

---
+++ fly-web/src/main/java/com/fyp/fly/web/clients/article/ArticleApiClient.java
@@ -0,0 +1,13 @@
+package com.fyp.fly.web.clients.article;
+
+import com.fyp.fly.common.result.api.JsonResult;
+import com.fyp.fly.web.controller.parameter.PostParameter;
+
+/**
+ * @author fyp
+ * @crate 2019/3/16 13:05
+ * @project fly
+ */
+public interface ArticleApiClient {
+    JsonResult add(PostParameter parameter);
+}
