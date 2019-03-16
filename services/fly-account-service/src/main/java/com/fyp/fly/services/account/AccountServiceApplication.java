dcc3aae8e09aa065da77a69e6a16b63e038bfe40
fanpan26 <fanpan26@126.com>
2019-03-16 15:04:40 +0800

    rename

--- services/fly-account-service/src/main/java/com/fyp/fly/services/account/AccountServiceApplication.java
+++ services/fly-account-service/src/main/java/com/fyp/fly/services/account/AccountServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@MapperScan("com.fyp.fly.services.account.repositories.mapper")
+@MapperScan("com.fyp.fly.services.account.repository.mapper")
 public class AccountServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
