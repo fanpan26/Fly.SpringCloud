b6bb072863c5379c0dee4b0c6c35778fe0bfd75d
fanpan26 <fanpan26@126.com>
2019-03-16 15:03:41 +0800

    Create FlyContext.java

---
+++ fly-web/src/main/java/com/fyp/fly/web/config/FlyContext.java
@@ -0,0 +1,54 @@
+package com.fyp.fly.web.config;
+
+import com.fyp.fly.common.constants.Fly;
+import com.fyp.fly.common.dto.FlyUserDto;
+import com.fyp.fly.common.utils.JSONUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author fyp
+ * @crate 2019/3/16 14:29
+ * @project fly
+ */
+@Component
+public class FlyContext {
+
+    private static RedisTemplate<String,String> redisTemplate;
+
+    @Autowired
+    public void setRedisTemplate(RedisTemplate<String,String> redisTemplate){
+        FlyContext.redisTemplate = redisTemplate;
+    }
+
+   public static HttpServletRequest getCurrentRequest(){
+       return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+   }
+
+    public static FlyUserDto getUser() {
+        FlyUserDto userFromAttribute = (FlyUserDto) getCurrentRequest().getAttribute(Fly.WEB_ATTRIBUTE_USER_KEY);
+        if (userFromAttribute == null) {
+            return null;
+        } else {
+            final String key = Fly.WEB_CACHE_USER_KEY + userFromAttribute.getId();
+            String userJSON = redisTemplate.opsForValue().get(key);
+            if (userJSON != null) {
+                return JSONUtils.parseObject(userJSON, FlyUserDto.class);
+            }
+        }
+        return new FlyUserDto();
+    }
+
+    public static Long getUserId() {
+        FlyUserDto userFromAttribute = (FlyUserDto) getCurrentRequest().getAttribute(Fly.WEB_ATTRIBUTE_USER_KEY);
+        if (userFromAttribute == null) {
+            return 0L;
+        }
+        return userFromAttribute.getId();
+    }
+}
