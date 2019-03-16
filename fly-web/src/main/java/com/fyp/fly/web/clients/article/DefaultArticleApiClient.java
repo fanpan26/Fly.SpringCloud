70a9e9fd9204071f59aef3687ba213bcdcbd52cf
fanpan26 <fanpan26@126.com>
2019-03-16 15:03:01 +0800

    Create DefaultArticleApiClient.java

---
+++ fly-web/src/main/java/com/fyp/fly/web/clients/article/DefaultArticleApiClient.java
@@ -0,0 +1,46 @@
+package com.fyp.fly.web.clients.article;
+
+import com.fyp.fly.common.result.api.JsonResult;
+import com.fyp.fly.web.clients.AbstractApiClient;
+import com.fyp.fly.web.config.FlyContext;
+import com.fyp.fly.web.controller.parameter.PostParameter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+
+/**
+ * @author fyp
+ * @crate 2019/3/16 13:06
+ * @project fly
+ */
+@Service
+public class DefaultArticleApiClient extends AbstractApiClient implements ArticleApiClient {
+
+
+    @Value("${gateway.services.article}")
+    private String serviceId;
+
+    @Override
+    protected String getServiceId() {
+        return serviceId;
+    }
+
+    private String getAddUrl(){
+        return buildApiUrl("article/");
+    }
+
+    @Override
+    public JsonResult add(PostParameter parameter) {
+       Long userId = FlyContext.getUserId();
+        MultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+        parameters.add("id", parameter.getId());
+        parameters.add("title", parameter.getTitle());
+        parameters.add("content", parameter.getContent());
+        parameters.add("category", parameter.getCategory());
+        parameters.add("experience", parameter.getExperience());
+
+        return postForObjectWithFormHeader(getAddUrl(), parameters);
+    }
+}
