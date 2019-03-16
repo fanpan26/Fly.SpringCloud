60fec71f8dee7793a88015479d1a825a940ff379
fanpan26 <fanpan26@126.com>
2019-03-16 15:02:21 +0800

    Update DateUtils.java

--- fly-common/src/main/java/com/fyp/fly/common/utils/DateUtils.java
+++ fly-common/src/main/java/com/fyp/fly/common/utils/DateUtils.java
@@ -1,7 +1,11 @@
 package com.fyp.fly.common.utils;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.date.DateUtil;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * @author fyp
@@ -9,14 +13,10 @@ import java.util.Date;
  * @project fly
  */
 public class DateUtils {
-    public static String timeStamp2Date(long seconds, String format) {
-        if (seconds <= 0){
+    public static String timeStamp2Date(long seconds) {
+        if (seconds <= 0) {
             return "";
         }
-        if (format == null || format.isEmpty()) {
-            format = "yyyy-MM-dd HH:mm:ss";
-        }
-        SimpleDateFormat sdf = new SimpleDateFormat(format);
-        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+        return DateUtil.formatDate(DateUtil.date(seconds * 1000));
     }
 }
