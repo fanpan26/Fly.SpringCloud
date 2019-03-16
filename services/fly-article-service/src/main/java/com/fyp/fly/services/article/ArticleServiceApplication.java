1e138d08c1b3c839eed55f622135fc17686028eb
fanpan26 <fanpan26@126.com>
2019-03-16 15:05:31 +0800

    Update ArticleServiceApplication.java

--- services/fly-article-service/src/main/java/com/fyp/fly/services/article/ArticleServiceApplication.java
+++ services/fly-article-service/src/main/java/com/fyp/fly/services/article/ArticleServiceApplication.java
@@ -1,5 +1,6 @@
 package com.fyp.fly.services.article;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @project fly
  */
 @SpringBootApplication
+@MapperScan("com.fyp.fly.services.article.repository.mapper")
 public class ArticleServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticleServiceApplication.class, args);
