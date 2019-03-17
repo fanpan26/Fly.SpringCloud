f2dc0da6182ad96a7a9ca537e95b1e200fc2a34c
fanpan26 <fanpan26@126.com>
2019-03-18 00:26:57 +0800

    Create UserApiClient.java

---
+++ fly-gateway/src/main/java/com/fyp/fly/gateway/client/UserApiClient.java
@@ -0,0 +1,23 @@
+package com.fyp.fly.gateway.client;
+
+import com.fyp.fly.common.dto.FlyUserDto;
+import com.fyp.fly.common.result.api.JsonResult;
+import com.fyp.fly.gateway.client.domain.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author fyp
+ * @crate 2019/3/17 21:23
+ * @project fly
+ */
+@FeignClient(name = "fly-user-service")
+public interface UserApiClient {
+    /**
+     * 获取用户信息
+     * */
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
+    JsonResult<User> getUserById(@PathVariable("id") Long id);
+}
