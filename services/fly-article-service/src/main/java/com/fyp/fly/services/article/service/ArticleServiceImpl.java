80d253087f2cf577c4077d6e2b73b089d169e471
fanpan26 <fanpan26@126.com>
2019-03-18 00:28:02 +0800

    Update ArticleServiceImpl.java

--- services/fly-article-service/src/main/java/com/fyp/fly/services/article/service/ArticleServiceImpl.java
+++ services/fly-article-service/src/main/java/com/fyp/fly/services/article/service/ArticleServiceImpl.java
@@ -1,7 +1,9 @@
 package com.fyp.fly.services.article.service;
 
+import com.fyp.fly.common.constants.Fly;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.common.result.api.ResultUtils;
+import com.fyp.fly.services.article.domain.Article;
 import com.fyp.fly.services.article.dto.ArticleEditDto;
 import com.fyp.fly.services.article.repository.mapper.ArticleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,5 +31,17 @@ public class ArticleServiceImpl implements ArticleService{
         }else{
             return ResultUtils.failed(article.checkArguments());
         }
+    }
+
+    @Override
+    public JsonResult findOneById(Long id) {
+        if (id == null || id == 0) {
+            throw new IllegalArgumentException("id");
+        }
+        Article article = articleMapper.findById(id);
+        if (article == null || article.getDel()){
+            return ResultUtils.failed(Fly.Status.API_CODE_NOTFOUND,"帖子不存在");
+        }
+        return ResultUtils.success(article);
     }
 }
