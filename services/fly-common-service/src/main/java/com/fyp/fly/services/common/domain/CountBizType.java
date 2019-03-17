1485d8e5ac966ec6b1835cb5ebd51865e595c73e
fanpan26 <fanpan26@126.com>
2019-03-17 12:42:42 +0800

    Update CountBizType.java

--- services/fly-common-service/src/main/java/com/fyp/fly/services/common/domain/CountBizType.java
+++ services/fly-common-service/src/main/java/com/fyp/fly/services/common/domain/CountBizType.java
@@ -9,27 +9,33 @@ public enum CountBizType {
     /**
      * 帖子浏览量
      * */
-    ARTICLE_BROWSER(1),
+    ARTICLE_BROWSER(1,"ab"),
     /**
      * 帖子评论量
      * */
-    ARTICLE__COMMENT(2),
+    ARTICLE__COMMENT(2,"ac"),
     /**
      * 评论点赞数
      * */
-    COMMENT_LIKE(3),
+    COMMENT_LIKE(3,"cl"),
     /**
      * 用户关注量
      * */
-    USER_FOLLOW(4);
+    USER_FOLLOW(4,"uf");
 
-    CountBizType(int code) {
+    CountBizType(int code,String key) {
         this.code = code;
+        this.key = key;
     }
 
+    private String key;
     private int code;
     public int getCode(){
         return code;
+    }
+
+    public String getKey() {
+        return key;
     }
 
     public static CountBizType valueOf(int code){
