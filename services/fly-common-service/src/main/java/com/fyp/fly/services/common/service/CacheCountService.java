21c2b9ae0061fb7a97467bbcb8b5cb090d484ab3
fanpan26 <fanpan26@126.com>
2019-03-17 12:42:53 +0800

    use string hash ops

--- services/fly-common-service/src/main/java/com/fyp/fly/services/common/service/CacheCountService.java
+++ services/fly-common-service/src/main/java/com/fyp/fly/services/common/service/CacheCountService.java
@@ -13,7 +13,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author fyp
@@ -27,7 +30,7 @@ public class CacheCountService implements CountService {
     private Long bucketSize;
 
     @Resource
-    private RedisTemplate<String,Object> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
     private static final String COUNT_KEY_PREFIX = "service:common:count:";
 
@@ -36,28 +39,63 @@ public class CacheCountService implements CountService {
      * Key: service:common:count:article_browse_1   k:bizId v:count
      * 10000001   34 (帖子ID 10000001，浏览数 34)
      * 10000002   58 (帖子ID 10000002，浏览数 58)
-     * */
-        private HashOperations<String, String, Object> countOps() {
+     */
+    private HashOperations<String, String, String> countOps() {
         return redisTemplate.opsForHash();
     }
 
-    private String getKeyByBizType(int type,Long bizId) {
-        String name = CountBizType.valueOf(type).name();
+    private String getKeyByBizType(int type, Long bizId) {
+        String key = CountBizType.valueOf(type).getKey();
         long sequence = bizId / bucketSize;
-        return String.format("%s%s_%d",COUNT_KEY_PREFIX,name,sequence);
+        return String.format("%s%s_%d", COUNT_KEY_PREFIX, key, sequence);
     }
 
+    /**
+     * 添加一条计数
+     */
     @Override
-    public JsonResult add(int type,Long bizId) {
+    public JsonResult add(int type, Long bizId) {
         String key = getKeyByBizType(type, bizId);
         //如果redis缓存丢失，会有数据不准的问题，不处理，交给计划去做，检查
-        Long res = countOps().increment(key, bizId.toString(), 1);
-        return ResultUtils.success();
+        Long count = countOps().increment(key, bizId.toString(), 1);
+        return ResultUtils.success(count);
     }
 
 
+    private static final JsonResult<List<CountDto>> EMPTY_COUNT_RESULT = ResultUtils.success(new ArrayList<CountDto>(0));
+
     @Override
-    public JsonResult<List<CountDto>> getListByBizIds(int bizType, long... bizIds) {
-        return null;
+    public JsonResult<List<CountDto>> getListByBizIds(int bizType, List<Long> bizIds) {
+        if (bizIds.size() == 0) {
+            return EMPTY_COUNT_RESULT;
+        }
+        //先计算key，在取值。
+        Map<String, List<String>> keyMaps = new HashMap<>(bizIds.size());
+        for (Long id : bizIds) {
+            String key = getKeyByBizType(bizType, id);
+            List<String> idList;
+            if (!keyMaps.containsKey(key)) {
+                idList = new ArrayList<>(bizIds.size());
+                idList.add(id.toString());
+
+            } else {
+                idList = keyMaps.get(key);
+                idList.add(id.toString());
+            }
+            keyMaps.put(key, idList);
+        }
+        List<CountDto> resultList = new ArrayList<>(bizIds.size());
+        for (Map.Entry<String, List<String>> entry : keyMaps.entrySet()) {
+            List<String> values = countOps().multiGet(entry.getKey(), entry.getValue());
+            if (values != null) {
+                for (int i = 0; i < values.size(); i++) {
+                    //ignore null value or use default 0 ?
+                    if (values.get(i) != null) {
+                        resultList.add(new CountDto(bizType, Long.valueOf(entry.getValue().get(i)), Integer.valueOf(values.get(i))));
+                    }
+                }
+            }
+        }
+        return ResultUtils.success(resultList);
     }
 }
