075b4a04cea6310a415c1d09ba0d78528166c146
fanpan26 <fanpan26@126.com>
2019-03-18 00:26:35 +0800

    Create ArticleApiClient.java

---
+++ fly-gateway/src/main/java/com/fyp/fly/gateway/client/ArticleApiClient.java
@@ -0,0 +1,23 @@
+package com.fyp.fly.gateway.client;
+
+import com.fyp.fly.common.dto.FlyUserDto;
+import com.fyp.fly.common.result.api.JsonResult;
+import com.fyp.fly.gateway.client.domain.Article;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author fyp
+ * @crate 2019/3/17 21:21
+ * @project fly
+ */
+@FeignClient(name = "fly-article-service")
+public interface ArticleApiClient {
+    /**
+     * 获取帖子信息
+     * */
+    @RequestMapping(value = "/article/{id}",method = RequestMethod.GET)
+    JsonResult<Article> getArticleById(@PathVariable("id") Long id);
+}
