5e35d43741f818267fc6e285823920b057beab31
fanpan26 <fanpan26@126.com>
2019-03-17 23:58:42 +0800

    Create CountBizType.java

---
+++ fly-common/src/main/java/com/fyp/fly/common/enums/CountBizType.java
@@ -0,0 +1,49 @@
+package com.fyp.fly.common.enums;
+
+/**
+ * @author fyp
+ * @crate 2019/3/17 22:29
+ * @project fly
+ */
+public enum CountBizType {
+    /**
+     * 帖子浏览量
+     * */
+    ARTICLE_BROWSER(1,"ab"),
+    /**
+     * 帖子评论量
+     * */
+    ARTICLE_COMMENT(2,"ac"),
+    /**
+     * 评论点赞数
+     * */
+    COMMENT_LIKE(3,"cl"),
+    /**
+     * 用户关注量
+     * */
+    USER_FOLLOW(4,"uf");
+
+    CountBizType(int code,String key) {
+        this.code = code;
+        this.key = key;
+    }
+
+    private String key;
+    private int code;
+    public int getCode(){
+        return code;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public static CountBizType valueOf(int code){
+        for (CountBizType e : CountBizType.values()){
+            if (e.getCode() == code){
+                return e;
+            }
+        }
+        throw new IllegalArgumentException("bizType with code "+ code +" does not exist");
+    }
+}
