f92cf3a6fc9cbac0c3a6e39be9749fe2ff0a8152
fanpan26 <fanpan26@126.com>
2019-03-17 12:42:36 +0800

    Update CountController.java

--- services/fly-common-service/src/main/java/com/fyp/fly/services/common/controller/CountController.java
+++ services/fly-common-service/src/main/java/com/fyp/fly/services/common/controller/CountController.java
@@ -4,10 +4,10 @@ import com.fyp.fly.common.dto.CountDto;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.services.common.service.CountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fyp
@@ -22,8 +22,23 @@ public class CountController {
     private CountService countService;
 
     @GetMapping("/{bizType}/{bizId}")
-    public CountDto getCountByBizId(@PathVariable("bizType") int type,@PathVariable("bizId") Long bizId){
-        countService.add(type,bizId);
-        return new CountDto();
+    public JsonResult getCountByBizId(@PathVariable("bizType") int type,@PathVariable("bizId") Long bizId) {
+        return countService.add(type, bizId);
+    }
+
+    @PostMapping("/{bizType}")
+    public JsonResult getCountsByBizIds(@PathVariable("bizType") int type,@RequestBody List<Long> bizIds) {
+        return countService.getListByBizIds(type, bizIds);
+    }
+
+    @GetMapping("/test")
+    public JsonResult getCountsByBizIdsTest() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        ids.add(2L);
+        ids.add(3L);
+        ids.add(4L);
+        ids.add(100001L);
+        return countService.getListByBizIds(1, ids);
     }
 }
