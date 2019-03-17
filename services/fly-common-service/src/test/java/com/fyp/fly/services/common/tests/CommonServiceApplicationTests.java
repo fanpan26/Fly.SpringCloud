df338d974e9d67ea6bf6c6adc355a9343791af20
fanpan26 <fanpan26@126.com>
2019-03-17 11:41:23 +0800

    Create CommonServiceApplicationTests.java

---
+++ services/fly-common-service/src/test/java/com/fyp/fly/services/common/tests/CommonServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fyp.fly.services.common.tests;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @author fyp
+ * @crate 2019/3/17 10:53
+ * @project fly
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CommonServiceApplicationTests.class)
+@WebAppConfiguration
+public class CommonServiceApplicationTests {
+}
