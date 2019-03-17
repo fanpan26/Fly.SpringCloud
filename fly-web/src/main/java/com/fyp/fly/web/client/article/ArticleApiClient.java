0ada4138b060b02bb7640bda57d41ee31abff427
fanpan26 <fanpan26@126.com>
2019-03-17 23:58:56 +0800

    Update ArticleApiClient.java

--- fly-web/src/main/java/com/fyp/fly/web/client/article/ArticleApiClient.java
+++ fly-web/src/main/java/com/fyp/fly/web/client/article/ArticleApiClient.java
@@ -3,6 +3,8 @@ package com.fyp.fly.web.client.article;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.web.controller.form.ArticleForm;
 
+import java.util.Map;
+
 /**
  * @author fyp
  * @crate 2019/3/16 13:05
@@ -10,4 +12,6 @@ import com.fyp.fly.web.controller.form.ArticleForm;
  */
 public interface ArticleApiClient {
     JsonResult add(ArticleForm parameter);
+
+    JsonResult<Map<String,Object>> getArticleById(Long articleId);
 }
