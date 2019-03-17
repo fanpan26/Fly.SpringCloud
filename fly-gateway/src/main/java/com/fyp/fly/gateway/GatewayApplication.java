dd0dbdda9fa75383eaf100c332a5a6a7b3cda5cd
fanpan26 <fanpan26@126.com>
2019-03-18 00:27:13 +0800

    Update GatewayApplication.java

--- fly-gateway/src/main/java/com/fyp/fly/gateway/GatewayApplication.java
+++ fly-gateway/src/main/java/com/fyp/fly/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.fyp.fly.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author fyp
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableFeignClients
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
