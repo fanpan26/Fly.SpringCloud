cc63b9bf7907b4bd499ca4f6e1aa54d635ef74d1
fanpan26 <fanpan26@126.com>
2019-03-21 22:32:44 +0800

    Update UserController.java

--- services/fly-user-service/src/main/java/com/fyp/fly/services/user/controller/UserController.java
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/controller/UserController.java
@@ -2,17 +2,19 @@ package com.fyp.fly.services.user.controller;
 
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.services.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author fyp
  * @crate 2019/3/1 22:53
  * @project fly
  */
+@Api(description = "用户账户接口")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -20,8 +22,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "获取用户信息")
     @GetMapping("/{id}")
     public JsonResult getUserInfoById(@PathVariable Long id){
         return userService.getUserInfo(id);
+    }
+
+    @ApiOperation(value = "获取用户列表")
+    @PostMapping("/list")
+    public JsonResult getUserList(@RequestParam("userIds") List<String> userIds) {
+        return userService.getUserList(userIds);
     }
 }
