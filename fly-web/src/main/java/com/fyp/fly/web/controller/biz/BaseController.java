5939cdf8625c47bded4fd8fc9a6bb4a9ea01d476
fanpan26 <fanpan26@126.com>
2019-03-21 22:31:50 +0800

    Update BaseController.java

--- fly-web/src/main/java/com/fyp/fly/web/controller/biz/BaseController.java
+++ fly-web/src/main/java/com/fyp/fly/web/controller/biz/BaseController.java
@@ -1,15 +1,9 @@
 package com.fyp.fly.web.controller.biz;
 
-import com.fyp.fly.common.constants.Fly;
-import com.fyp.fly.common.dto.FlyUserDto;
-import com.fyp.fly.common.utils.JSONUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * @author fyp
