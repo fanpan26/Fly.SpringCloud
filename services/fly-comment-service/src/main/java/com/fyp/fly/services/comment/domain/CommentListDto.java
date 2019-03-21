f438be87df04bf7a4c08ece3f077abe5afca84b7
fanpan26 <fanpan26@126.com>
2019-03-21 22:32:24 +0800

    Create CommentListDto.java

---
+++ services/fly-comment-service/src/main/java/com/fyp/fly/services/comment/domain/CommentListDto.java
@@ -0,0 +1,69 @@
+package com.fyp.fly.services.comment.domain;
+
+/**
+ * @author fyp
+ * @crate 2019/3/21 22:00
+ * @project fly
+ */
+public class CommentListDto {
+    private Long artId;
+    private Long authorId;
+    private Long userId;
+
+    public Long getArtId() {
+        return artId;
+    }
+
+    public void setArtId(Long artId) {
+        this.artId = artId;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    private Integer pageIndex;
+    private Integer pageSize;
+
+    public Integer getStart(){
+        if (pageIndex== null||pageIndex<0){
+            pageIndex=1;
+        }
+        if (pageSize==null||pageSize<=0||pageSize>100) {
+            pageSize = 20;
+        }
+        return (pageIndex-1)*pageSize +1;
+    }
+
+    public Integer getEnd(){
+        return getStart()+ pageSize;
+    }
+}
