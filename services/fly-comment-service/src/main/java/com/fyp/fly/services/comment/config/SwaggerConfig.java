6251f3cb3026e957e91fb16bfe37d383a32cc41a
fanpan26 <fanpan26@126.com>
2019-03-21 22:32:14 +0800

    Create SwaggerConfig.java

---
+++ services/fly-comment-service/src/main/java/com/fyp/fly/services/comment/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.fyp.fly.services.comment.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author fyp
+ * @crate 2019/3/21 22:11
+ * @project fly
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+    }
+}
