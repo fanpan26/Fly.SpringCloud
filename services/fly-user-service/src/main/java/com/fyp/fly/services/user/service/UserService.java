8fda878153092bf47e0b6ace6f39db73fa9f1290
fanpan26 <fanpan26@126.com>
2019-03-21 22:32:54 +0800

    Update UserService.java

--- services/fly-user-service/src/main/java/com/fyp/fly/services/user/service/UserService.java
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.fyp.fly.services.user.service;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.services.user.domain.User;
 
+import java.util.List;
+
 /**
  * @author fyp
  * @crate 2019/3/13 20:10
@@ -13,6 +15,8 @@ public interface UserService {
     /**
      * 获取用户信息
      * */
-    JsonResult<User> getUserInfo(Long userId);
+    JsonResult getUserInfo(Long userId);
+
+    JsonResult getUserList(List<String> userIds);
 
 }
