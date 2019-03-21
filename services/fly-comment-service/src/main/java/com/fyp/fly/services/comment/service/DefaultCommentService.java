1e9509d7fe85dec69c5e30ad0e0b7eeeaed1601d
fanpan26 <fanpan26@126.com>
2019-03-21 22:32:31 +0800

    Update DefaultCommentService.java

--- services/fly-comment-service/src/main/java/com/fyp/fly/services/comment/service/DefaultCommentService.java
+++ services/fly-comment-service/src/main/java/com/fyp/fly/services/comment/service/DefaultCommentService.java
@@ -1,14 +1,17 @@
 package com.fyp.fly.services.comment.service;
 
+import com.fyp.fly.common.dto.UserModel;
 import com.fyp.fly.common.enums.CountBizType;
 import com.fyp.fly.common.event.CountEvent;
 import com.fyp.fly.common.event.FlyEvent;
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.common.result.api.ResultUtils;
 import com.fyp.fly.common.utils.JSONUtils;
+import com.fyp.fly.services.comment.client.UserFeignClient;
 import com.fyp.fly.services.comment.domain.Comment;
 import com.fyp.fly.services.comment.domain.CommentCache;
 import com.fyp.fly.services.comment.domain.CommentDto;
+import com.fyp.fly.services.comment.domain.CommentListDto;
 import com.fyp.fly.services.comment.repository.mapper.CommentMapper;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,10 +21,15 @@ import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class DefaultCommentService implements CommentService {
+
+    private static final String CACHE_COMMENT_LIST = "service:comment:l_";
 
     @Autowired
     private ListOperations<String, Object> listOps;
@@ -32,6 +40,9 @@ public class DefaultCommentService implements CommentService {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private UserFeignClient userFeignClient;
 
     @Override
     public JsonResult add(CommentDto comment) {
@@ -51,7 +62,7 @@ public class DefaultCommentService implements CommentService {
     @Override
     public JsonResult addCache(CommentDto comment) {
         Comment commentModel = comment.transfer();
-        listOps.leftPush("service:comment:l_" + comment.getArtId(), commentModel);
+        listOps.leftPush(CACHE_COMMENT_LIST + comment.getArtId(), commentModel);
         //List<Object> list = listOps.range("service:comment:l_" + comment.getArtId(),1,20);
         return ResultUtils.success();
     }
@@ -86,6 +97,26 @@ public class DefaultCommentService implements CommentService {
         }
         commentMapper.updateContent(id,uid,content);
         return ResultUtils.success();
+    }
+
+    @Override
+    public JsonResult getList(CommentListDto listParam) {
+        List<Object> commentList = listOps.range(CACHE_COMMENT_LIST + listParam.getArtId(), listParam.getStart(), listParam.getEnd());
+
+        List<Comment> comments = commentList.stream().map(c -> JSONUtils.parseObject(c.toString(), Comment.class)).collect(Collectors.toList());
+        List<Long> userIds = comments.stream().map(x -> x.getUid()).distinct().collect(Collectors.toList());
+        //根据用户ID获取用户集合
+        JsonResult<List<UserModel>> userList = userFeignClient.getList(userIds);
+        if (ResultUtils.isSuccess(userList)) {
+            comments.stream().forEach(c ->{
+                c.setUser(userList.getData().stream().filter(x -> c.getUid() == x.getId()).findFirst().get());
+                // if user is article author
+                c.setAuthor(Objects.equals(c.getUid(),listParam.getAuthorId()));
+                // if user is comment editor
+                c.setEditor(Objects.equals(c.getUid(),listParam.getUserId()));
+            });
+        }
+        return ResultUtils.success(comments);
     }
 
     private void sendCommentCountChangedEvent(Long id,boolean increment) {
