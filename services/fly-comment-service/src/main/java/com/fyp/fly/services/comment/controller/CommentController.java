c84595b3029a6381f69f92d4b968e15b4e488577
fanpan26 <fanpan26@126.com>
2019-03-21 22:32:17 +0800

    Update CommentController.java

--- services/fly-comment-service/src/main/java/com/fyp/fly/services/comment/controller/CommentController.java
+++ services/fly-comment-service/src/main/java/com/fyp/fly/services/comment/controller/CommentController.java
@@ -2,10 +2,14 @@ package com.fyp.fly.services.comment.controller;
 
 import com.fyp.fly.common.result.api.JsonResult;
 import com.fyp.fly.services.comment.domain.CommentDto;
+import com.fyp.fly.services.comment.domain.CommentListDto;
 import com.fyp.fly.services.comment.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "用户账户接口")
 @RestController
 @RequestMapping("comment")
 public class CommentController {
@@ -14,23 +18,33 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+    @ApiOperation(value = "添加评论")
     @PostMapping("/")
     public JsonResult add(CommentDto comment) {
         return commentService.addCache(comment);
     }
 
+    @ApiOperation(value = "删除评论")
     @PostMapping("/remove/{id}")
     public JsonResult remove(@PathVariable("id") Long id) {
         return commentService.delete(id);
     }
 
+    @ApiOperation(value = "获取评论内容")
     @GetMapping("/content/{id}")
     public JsonResult getContent(@PathVariable("id") Long id) {
         return commentService.getContent(id);
     }
 
+    @ApiOperation(value = "更新评论内容")
     @PostMapping("/content/{id}")
     public JsonResult updateContent(@PathVariable("id") Long id,Long userId,String content){
         return commentService.updateContent(id,userId,content);
+    }
+
+    @ApiOperation(value = "获取评论列表")
+    @GetMapping("/list")
+    public JsonResult getList(CommentListDto listParam) {
+        return commentService.getList(listParam);
     }
 }
