afe66df318bf55174d64aebfd43a5810d5a6089f
fanpan26 <fanpan26@126.com>
2019-03-24 14:57:50 +0800

    Update ArticleServiceApplication.java

--- services/fly-article-service/src/main/java/com/fyp/fly/services/article/ArticleServiceApplication.java
+++ services/fly-article-service/src/main/java/com/fyp/fly/services/article/ArticleServiceApplication.java
@@ -3,6 +3,7 @@ package com.fyp.fly.services.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author fyp
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @project fly
  */
 @SpringBootApplication
+@EnableFeignClients
 @MapperScan("com.fyp.fly.services.article.repository.mapper")
 public class ArticleServiceApplication {
     public static void main(String[] args) {
