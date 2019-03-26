2054dd06acce35758a2231475aa97087adb1b346
fanpan26 <fanpan26@126.com>
2019-03-26 22:37:37 +0800

    Create CountFeignClient.java

---
+++ services/fly-user-service/src/main/java/com/fyp/fly/services/user/client/CountFeignClient.java
@@ -0,0 +1,28 @@
+package com.fyp.fly.services.user.client;
+
+import com.fyp.fly.common.dto.CountVo;
+import com.fyp.fly.common.result.api.JsonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author fyp
+ * @crate 2019/3/26 21:21
+ * @project fly
+ */
+@FeignClient(name = "fly-common-service")
+public interface CountFeignClient {
+    /**
+     * 获取TOP N 的数据统计
+     * */
+    @RequestMapping(value = "/count/top/{bizType}",method = RequestMethod.GET)
+    JsonResult<List<CountVo>> getTopNCountsByBizType(@PathVariable("bizType") Integer bizType, @RequestParam("start") Integer start, @RequestParam("end") Integer end);
+
+    /**
+     * 根据ID列表获取对应的个数
+     * */
+    @RequestMapping(value = "/count/type/{bizType}",method = RequestMethod.GET)
+    JsonResult<List<CountVo>> getCountsByBizIds(@PathVariable("bizType") int type,@RequestBody List<Long> bizIds);
+}
